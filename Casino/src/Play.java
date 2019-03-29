fd29630819efb0fe43228d362bafc4af81218a04
jzhou908 <43217641+jzhou908@users.noreply.github.com>
2019-03-29 14:43:42 -0400

    Update Play.java

--- Casino/src/Play.java
+++ Casino/src/Play.java
@@ -4,24 +4,28 @@ public class Play{
 	public static void main(String args[]) {
 		Blackjack bj = new Blackjack();
 		Scanner in = new Scanner(System.in);
-		System.out.println("How much money do you have?");
-		int balance = in.nextInt();
-		in.nextLine();
-		System.out.println("How much money do you wanna bet?");
-		int bet = in.nextInt();
-		in.nextLine();
-		if(bet>balance) {
-			System.out.println("Please bet an amount less than your balance.");
-			return;
+		boolean again = true;
+		bj.start();
+		while(again) {
+			bj.newgame();
+			bj.print();
+			if(bj.hitorstandordoubledown()) {
+				bj.win();
+				bj.printwin();
+				bj.profit();
+				System.out.println("Do you want to play again? (y/n)");
+				String a = in.nextLine();
+				if(a.equals("y")){
+					again = true;
+				}
+				else if(a.equals("n")){
+					again = false;
+				}
+			}
+			else {
+				again = false;
+			}
 		}
-		bj.newgame(bet);
-		bj.print();
-		System.out.println("Do you wanna hit or stand?");
-		String answer = in.nextLine();
-		bj.hitorstand(answer);
-		bj.win();
-		bj.printwin();
-		bj.profit(bet);
 		in.close();
 	}
 }
